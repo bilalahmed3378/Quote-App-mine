35e87e533ee091d126a172d3e72adb955fc38bef
bilalahmed3378 <109059774+bilalahmed3378@users.noreply.github.com>
2023-03-24 16:55:42 +0500

    Initial Commit

---
+++ Quote App/Quote_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quote_AppApp.swift
+//  Quote App
+//
+//  Created by Bilal Ahmed on 24/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Quote_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
